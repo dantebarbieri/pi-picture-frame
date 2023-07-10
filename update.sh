234112a38dbd004a63eb902f6d67e4da9ec9367c
dantebarbieri <code@dantebarbieri.dev>
2023-07-09 20:25:58 -0500

    Improve update

--- update.sh
+++ update.sh
@@ -2,16 +2,17 @@
 set -e
 
 echo "Getting the directory of the script..."
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 echo "The script directory is: ${SCRIPT_DIR}"
 pushd "${SCRIPT_DIR}"
 
 echo "Updating scripts..."
 # Update all scripts, but **run.sh** & **update.sh** are ALWAYS the entrypoints
-echo "Resetting local changes..."
-git reset origin/HEAD --hard
-echo "Pulling latest changes from main branch..."
-git pull origin main
+echo "Discarding local changes..."
+git checkout -- .
+echo "Resetting branch to origin/main..."
+git fetch origin main
+git reset --hard origin/main
 echo "Scripts updated successfully."
 
 popd
