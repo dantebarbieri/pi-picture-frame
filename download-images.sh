099789fd31ed8a33509020ecc126ad0052ca54c4
Dante Barbieri <code@dantebarbieri.dev>
2023-07-11 16:08:18 -0500

    Change album to node & point at gallery

--- download-images.sh
+++ download-images.sh
@@ -9,8 +9,8 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 pushd "${SCRIPT_DIR}"
 
-echo "Getting album id..."
-source get-album-names.sh
+# echo "Getting album id..."
+# source get-album-names.sh
 
 echo "Loading environment variables..."
 set -o allexport
@@ -18,20 +18,29 @@ source .env || true
 source .config
 set +o allexport
 
-ALBUM_DIR="${SCRIPT_DIR}/images/${ALBUM_NAME}"
-echo "Creating album directory at: ${ALBUM_DIR}"
-mkdir -p "${ALBUM_DIR}"
-pushd "${ALBUM_DIR}"
+NODE_DIR="${SCRIPT_DIR}/images/${NODE_NAME}"
+echo "Creating album directory at: ${NODE_DIR}"
+mkdir -p "${NODE_DIR}"
+pushd "${NODE_DIR}"
 
 echo "Getting API key and secret from environment variables..."
 API_KEY=${SMUGMUG_API_KEY}
 API_SECRET=${SMUGMUG_API_SECRET}
 
 echo "Constructing URL..."
-URL="https://api.smugmug.com/api/v2/album/${ALBUM_ID}!images?APIKey=${API_KEY}"
+NODE_URL="https://api.smugmug.com/api/v2/node/${NODE_NAME}?APIKey=${API_KEY}"
 
-echo "Making request to SmugMug API..."
-response=$(curl -s -H "Accept: application/json" -H "Authorization: Basic $(echo -n "${API_KEY}:${API_SECRET}" | base64 --wrap=0)" -X GET ${URL})
+echo -n "Making request to SmugMug API @ ${NODE_URL}..."
+node_response=$(curl -s -H "Accept: application/json" -H "Authorization: Basic $(echo -n "${API_KEY}:${API_SECRET}" | base64)" -X GET ${NODE_URL})
+echo "$(echo -n "$node_response" | wc -c) bytes received."
+
+ALBUM_URI=$(echo $node_response | jq -r '.Response.Node.Uris.Album.Uri')
+
+ALBUM_URL="https://api.smugmug.com${ALBUM_URI}!images?APIKey=${API_KEY}"
+
+echo -n "Making request to SmugMug API @ ${ALBUM_URL}..."
+response=$(curl -s -H "Accept: application/json" -H "Authorization: Basic $(echo -n "${API_KEY}:${API_SECRET}" | base64)" -X GET ${ALBUM_URL})
+echo "$(echo -n "$response" | wc -c) bytes received."
 
 echo "Parsing the response..."
 mapfile -t image_data < <(echo $response | jq -r '.Response.AlbumImage[] | "\(.FileName)=\(.ArchivedUri)"')
@@ -71,7 +80,7 @@ allowed_keywords=($(IFS=":"; echo $ALLOWED_KEYWORDS))
 echo "Creating symbolic links for images that match allowed keywords..."
 for keyword in "${allowed_keywords[@]}"
 do
-    for img in $(exiftool -q -r -if '$Keywords =~ /'"$keyword"'/i' -p '$Directory/$FileName' "$ALBUM_DIR")
+    for img in $(exiftool -q -r -if '$Keywords =~ /'"$keyword"'/i' -p '$Directory/$FileName' "$NODE_DIR")
     do
         ln -s "$img" "$IMAGES_DIR"
     done
